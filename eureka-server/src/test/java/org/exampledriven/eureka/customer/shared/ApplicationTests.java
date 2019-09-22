22114e1d0375687451ebca864350928f069e22b2
desaiajayhnr <38744856+desaiajayhnr@users.noreply.github.com>
2019-09-22 00:31:37 -0700

    test

--- eureka-server/src/test/java/org/exampledriven/eureka/customer/shared/ApplicationTests.java
+++ eureka-server/src/test/java/org/exampledriven/eureka/customer/shared/ApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//Test
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class ApplicationTests {
